dbc5078d3aace104d433f0da28756948ca168be6
taoufikkaabi <taoufikkaabi@insat.u-carthage.tn>
2021-05-25 10:51:23 +0100

    login/logout

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,15 +4,25 @@ namespace App\DataFixtures;
 use App\Entity\Operateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class UserFixture extends Fixture
 {
+    private $passwordEncoder;
+    /**
+     * UserFixtures constructor.
+     */
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->passwordEncoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
        for($i=1;$i<5;$i++)
        { $user = new Operateur();
           $user->setEmail("admin$i@insat.u-carthage.tn");
-          $user->setPassword("admin");
+          $user->setPassword(
+              $this->passwordEncoder->encodePassword($user, 'admin')
+          );
           $user->setNom("admin$i");
           $user->setPrenom("admin$i");
           $manager->persist($user);
--- src/Security/UserAuthAuthenticator.php
+++ src/Security/UserAuthAuthenticator.php
@@ -94,9 +94,7 @@ class UserAuthAuthenticator extends AbstractFormLoginAuthenticator implements Pa
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
-
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('actualites'));
     }
 
     protected function getLoginUrl()
