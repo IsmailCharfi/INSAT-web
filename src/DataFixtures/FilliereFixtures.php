9cd747d5793775700ecc15dd804162ef931011eb
motez-baccouch <80569257+motez-baccouch@users.noreply.github.com>
2021-05-25 22:04:20 +0100

    Create FilliereFixtures.php

---
+++ src/DataFixtures/FilliereFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Filiere;
+use App\Entity\Matiere;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class FilliereFixtures extends Fixture implements FixtureGroupInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $tfiliere = [1 => "MPI", 2 => "CBA", 3 => "GL", 4 => "RT", 5 => "IA", 6 => "IMI", 7 => "CH", 8 => "BIO"];
+        foreach ($tfiliere as $key => $value){
+            $filiere = new Filiere();
+        $filiere->setFiliere($value);
+        $filiere->setOrdre($key);
+        $manager->persist($filiere);
+        }
+        $matiere=new Matiere();
+        $matiere->setNom("Web");
+        $manager->persist($matiere);
+        $matiere=new Matiere();
+        $matiere->setNom("Algebre");
+        $manager->persist($matiere);
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        // TODO: Implement getGroups() method.
+        return ['groupeFiliere'];
+    }
+}
