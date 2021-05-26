a8fa6137f9268d33f90c8c6b8933ecf90a75f78d
motez-baccouch <80569257+motez-baccouch@users.noreply.github.com>
2021-05-26 22:59:08 +0100

    scolarité fonctionalites

--- src/Controller/ScolariteController.php
+++ src/Controller/ScolariteController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+
+use App\Form\ScolariteType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class ScolariteController extends AbstractController
 {
     #[Route('/scolarite', name: 'scolarite')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $repository = $this->getDoctrine()->getRepository('App:Filiere');
+        $filiere = $repository->findall();
+
         return $this->render('scolarite/index.html.twig', [
             'controller_name' => 'ScolariteController',
+            'filiere'=>$filiere
+
+        ]);
+    }
+
+
+
+    #[Route('/notes}', name: 'notes')]
+    public function notes(Request $request): Response
+    {
+
+
+
+        return $this->render('etudiant/index.html.twig', [
+            'controller_name' => 'ScolariteController',
+            'form' => $form->createView()
         ]);
     }
 }
